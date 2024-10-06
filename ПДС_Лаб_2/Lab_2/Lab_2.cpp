33b6890065e45f5bf1c24f7af65202e1c3b26023
Nazar <116120344+martsinko@users.noreply.github.com>
2024-10-06 16:00:52 +0300

    Add files via upload

---
+++ ПДС_Лаб_2/Lab_2/Lab_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+void truthTable() {
+    bool p, q, r;
+
+    cout << "p\tq\tr\tq ~ r\tp -> (q ~ r)" << endl;
+    cout << "-----------------------------------" << endl;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                p = i;  
+                q = j;
+                r = k;  
+
+                bool not_r = !r;            
+                bool q_and_not_r = q && not_r; 
+                bool result = !p || q_and_not_r; 
+
+                cout << p << "\t" << q << "\t" << r << "\t" << q_and_not_r << "\t" << result << endl;
+            }
+        }
+    }
+}
+
+int main() {
+    truthTable();
+    return 0;
+}
---
+++ ПДС_Лаб_2/Lab_2/UnitTestLab_2/UnitTestLab_2.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab2
+{
+	bool implication(bool p, bool q_and_not_r) {
+		return !p || q_and_not_r;
+	}
+
+	TEST_CLASS(UnitTestLab2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			// Тестування різних комбінацій значень для p, q ~ r
+			Assert::AreEqual(implication(0, 1), true);  // p = 0, q ~ r = 1 -> p -> (q ~ r) = 1
+			Assert::AreEqual(implication(0, 0), true);  // p = 0, q ~ r = 0 -> p -> (q ~ r) = 1
+			Assert::AreEqual(implication(1, 1), true);  // p = 1, q ~ r = 1 -> p -> (q ~ r) = 1
+			Assert::AreEqual(implication(1, 0), false); // p = 1, q ~ r = 0 -> p -> (q ~ r) = 0
+		}
+	};
+}
