093381985752c0316f388b1f1f98acf2922b0c5a
Nazar <116120344+martsinko@users.noreply.github.com>
2024-10-21 02:25:35 +0300

    Add files via upload

---
+++ ПДС_Лаб_6/Lab_6_1/Lab_6_1.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <Windows.h>
+
+struct Graph {
+    int n; // Кількість вершин
+    int m; // Кількість ребер
+    std::vector<std::pair<int, int>> edges;
+};
+
+Graph readGraphFromFile(const std::string& filename) {
+    Graph graph;
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Не вдалося відкрити файл");
+    }
+
+    file >> graph.n >> graph.m;
+    graph.edges.resize(graph.m);
+
+    for (int i = 0; i < graph.m; ++i) {
+        file >> graph.edges[i].first >> graph.edges[i].second;
+    }
+
+    file.close();
+    return graph;
+}
+
+void printAdjacencyMatrix(const Graph& graph) {
+    std::vector<std::vector<int>> adjacencyMatrix(graph.n, std::vector<int>(graph.n, 0));
+
+    for (const auto& edge : graph.edges) {
+        adjacencyMatrix[edge.first - 1][edge.second - 1] = 1; 
+    }
+
+    std::cout << "Матриця суміжності:\n";
+    for (const auto& row : adjacencyMatrix) {
+        for (int value : row) {
+            std::cout << value << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+void printIncidenceMatrix(const Graph& graph) {
+    std::vector<std::vector<int>> incidenceMatrix(graph.n, std::vector<int>(graph.m, 0));
+
+    for (int i = 0; i < graph.m; ++i) {
+        incidenceMatrix[graph.edges[i].first - 1][i] = 1;  
+        incidenceMatrix[graph.edges[i].second - 1][i] = -1; 
+    }
+
+    std::cout << "Матриця інцидентності:\n";
+    for (const auto& row : incidenceMatrix) {
+        for (int value : row) {
+            std::cout << value << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+void writeMatrixToFile(const std::string& filename, const std::vector<std::vector<int>>& matrix) {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Не вдалося відкрити файл для запису");
+    }
+
+    for (const auto& row : matrix) {
+        for (int value : row) {
+            file << value << " ";
+        }
+        file << "\n";
+    }
+
+    file.close();
+}
+
+void writeAdjacencyMatrixToFile(const std::string& filename, const Graph& graph) {
+    std::vector<std::vector<int>> adjacencyMatrix(graph.n, std::vector<int>(graph.n, 0));
+
+    for (const auto& edge : graph.edges) {
+        adjacencyMatrix[edge.first - 1][edge.second - 1] = 1; 
+    }
+
+    writeMatrixToFile(filename, adjacencyMatrix);
+}
+
+void writeIncidenceMatrixToFile(const std::string& filename, const Graph& graph) {
+    std::vector<std::vector<int>> incidenceMatrix(graph.n, std::vector<int>(graph.m, 0));
+
+    for (int i = 0; i < graph.m; ++i) {
+        incidenceMatrix[graph.edges[i].first - 1][i] = 1;  
+        incidenceMatrix[graph.edges[i].second - 1][i] = -1;
+    }
+
+    writeMatrixToFile(filename, incidenceMatrix);
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    try {
+        const std::string inputDirname = "D:\\Лаби ДПС\\ПДС_Лаб_6\\";
+        const std::string inputFilename = "D:\\Лаби ДПС\\ПДС_Лаб_6\\graph.txt";
+        Graph graph = readGraphFromFile(inputFilename);
+
+        printAdjacencyMatrix(graph);
+        printIncidenceMatrix(graph);
+
+        std::string outputAdjMatrixFilename, outputIncMatrixFilename;
+        std::cout << "Введіть ім'я файлу для запису матриці суміжності: ";
+        std::cin >> outputAdjMatrixFilename;
+        writeAdjacencyMatrixToFile(inputDirname + outputAdjMatrixFilename, graph);
+
+        std::cout << "Введіть ім'я файлу для запису матриці інцидентності: ";
+        std::cin >> outputIncMatrixFilename;
+        writeIncidenceMatrixToFile(inputDirname + outputIncMatrixFilename, graph);
+
+        std::cout << "Матриці суміжності та інцидентності виведені на екран та записані у файли." << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Помилка: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ ПДС_Лаб_6/UnitTestLab_6_1/UnitTestLab_6_1.cpp
@@ -0,0 +1,85 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_6_1/Lab_6_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab61
+{
+	TEST_CLASS(UnitTestLab61)
+	{
+	public:
+		
+        TEST_METHOD(TestReadGraphFromFile)
+        {
+
+            const std::string testFilename = "test_graph.txt";
+            {
+                std::ofstream outFile(testFilename);
+                outFile << "3 3\n1 2\n2 3\n3 1\n"; 
+                outFile.close();
+            }
+
+            Graph graph = readGraphFromFile(testFilename);
+
+            Assert::AreEqual(3, graph.n);
+            Assert::AreEqual(3, graph.m);
+            Assert::AreEqual(3, (int)graph.edges.size());
+            Assert::AreEqual(1, graph.edges[0].first);
+            Assert::AreEqual(2, graph.edges[0].second);
+
+            std::remove(testFilename.c_str());
+        }
+
+        TEST_METHOD(TestAdjacencyMatrix)
+        {
+            Graph graph;
+            graph.n = 3;
+            graph.m = 3;
+            graph.edges = { {1, 2}, {2, 3}, {3, 1} };
+
+            std::vector<std::vector<int>> expectedAdjacencyMatrix = {
+                {0, 1, 0},
+                {0, 0, 1},
+                {1, 0, 0}
+            };
+
+            std::vector<std::vector<int>> actualAdjacencyMatrix(graph.n, std::vector<int>(graph.n, 0));
+            for (const auto& edge : graph.edges) {
+                actualAdjacencyMatrix[edge.first - 1][edge.second - 1] = 1;
+            }
+
+            for (size_t i = 0; i < expectedAdjacencyMatrix.size(); ++i) {
+                for (size_t j = 0; j < expectedAdjacencyMatrix[i].size(); ++j) {
+                    Assert::AreEqual(expectedAdjacencyMatrix[i][j], actualAdjacencyMatrix[i][j]);
+                }
+            }
+        }
+
+        TEST_METHOD(TestIncidenceMatrix)
+        {
+            Graph graph;
+            graph.n = 3;
+            graph.m = 3;
+            graph.edges = { {1, 2}, {2, 3}, {3, 1} };
+
+            std::vector<std::vector<int>> expectedIncidenceMatrix = {
+                {1, 0, 1},
+                {-1, 1, 0},
+                {0, -1, 1}
+            };
+
+            std::vector<std::vector<int>> actualIncidenceMatrix(graph.n, std::vector<int>(graph.m, 0));
+            for (int i = 0; i < graph.m; ++i) {
+                actualIncidenceMatrix[graph.edges[i].first - 1][i] = 1;
+                actualIncidenceMatrix[graph.edges[i].second - 1][i] = -1;
+            }
+
+            for (size_t i = 0; i < expectedIncidenceMatrix.size(); ++i) {
+                for (size_t j = 0; j < expectedIncidenceMatrix[i].size(); ++j) {
+                    Assert::AreEqual(expectedIncidenceMatrix[i][j], actualIncidenceMatrix[i][j]);
+                }
+            }
+        }
+	};
+}
